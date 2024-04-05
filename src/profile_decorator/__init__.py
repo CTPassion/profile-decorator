d57e6b1b26c6fd578d96943a8c14ae891c21f0d8
J01024 <J01024@uniper.energy>
2024-04-05 21:43:16 +0100

    added async support, unit tests

--- src/profile_decorator/__init__.py
+++ src/profile_decorator/__init__.py
@@ -1,5 +1,7 @@
 # pylint: disable = missing-module-docstring,missing-function-docstring
+import asyncio
 import cProfile
+import logging
 import pstats
 from pstats import SortKey
 import io
@@ -13,49 +15,65 @@ VALID_SORTS = set(
 )
 
 
-def profile_me(n_rows=50, sort_by="cumulative", output="stdout", filename=None):
+def profile(n_rows=50, sort_by="cumulative", output="stdout", filename=None):
 
-    valid_outputs = {"stdout", "file"}
+    valid_outputs = {"stdout", "file", "log"}
 
     def decorator(func):
+
         @wraps(func)
-        def wrapper(*args, **kwargs):
-            pr = cProfile.Profile()
-            pr.enable()
-
-            # Execute the decorated function
-            result = func(*args, **kwargs)
-
-            pr.disable()
-            s = io.StringIO()
-
-            # Fallback to 'cumulative' if sort_by is invalid
-            sort_by_valid = sort_by if sort_by in VALID_SORTS else "cumulative"
-            ps = pstats.Stats(pr, stream=s).sort_stats(sort_by_valid)
-            ps.print_stats(n_rows)
-
-            # Handle output
-            if output not in valid_outputs:
-                raise ValueError(
-                    f"Invalid output option '{output}'. Valid options are {valid_outputs}."
-                )
-
-            if output == "stdout":
-                print(s.getvalue())
-            elif output == "file":
-                if not filename:
-                    raise ValueError("Filename must be provided when output is 'file'.")
-                try:
-                    with open(filename, "w+", encoding="utf-8") as f:
-                        f.write(s.getvalue())
-                except IOError as e:
-                    raise IOError(f"Error writing to file {filename}: {e}") from e
-            else:
-                raise ValueError("'output' must be one of 'stdout' or 'file'.")
+        async def async_wrapper(*args, **kwargs):
+            with cProfile.Profile() as pr:
+                # Directly await the async function here
+                result = await func(*args, **kwargs)
+                process_profiling_results(pr)
 
             return result
 
-        return wrapper
+        @wraps(func)
+        def sync_wrapper(*args, **kwargs):
+            with cProfile.Profile() as pr:
+                result = func(*args, **kwargs)
+                process_profiling_results(pr)
+
+            return result
+
+        if asyncio.iscoroutinefunction(func):
+            return async_wrapper
+        return sync_wrapper
+
+    def process_profiling_results(pr):
+        s = io.StringIO()
+        # Fallback to 'cumulative' if sort_by is invalid
+        if isinstance(sort_by, (list, tuple)):
+            sort_by_valid = [s for s in sort_by if s in VALID_SORTS]
+        else:
+            sort_by_valid = [sort_by] if sort_by in VALID_SORTS else ["cumulative"]
+
+        ps = pstats.Stats(pr, stream=s).sort_stats(*sort_by_valid)
+        ps.print_stats(n_rows)
+
+        # Handle output
+        if output not in valid_outputs:
+            raise ValueError(
+                f"Invalid output option '{output}'. Valid options are {valid_outputs}."
+            )
+
+        if output == "stdout":
+            print(s.getvalue())
+        elif output == "file":
+            if not filename:
+                raise ValueError("Filename must be provided when output is 'file'.")
+            try:
+                with open(filename, "w+", encoding="utf-8") as f:
+                    f.write(s.getvalue())
+            except IOError as e:
+                raise IOError(f"Error writing to file {filename}: {e}") from e
+        elif output == "log":
+            logger = logging.getLogger(__name__)
+            logger.info(s.getvalue())
+        else:
+            raise ValueError("'output' must be one of 'stdout', 'file' or 'log'.")
 
     # Enable the decorator to be used without parentheses if no arguments are provided
     if callable(n_rows):
--- src/profile_decorator/version.py
+++ src/profile_decorator/version.py
@@ -5,7 +5,8 @@ History:
 
 0.0.1   First release.
 0.0.2   New documentation added.
-0.0.3   workflows added
+0.0.3   Workflows added
+0.0.4   Renamed decorator, added async support, added unit tests
 """
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
